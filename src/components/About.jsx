545c071f84051483c738350218bbc32d846435f6
uniflynArjun <uniflyn.arjun@gmail.com>
2024-03-14 00:39:17 +0530

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,29 +9,19 @@ const About = () => {
     <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
       <div className="pb-5">
         <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-          About
+          About me
         </p>
       </div>
 
       <p className="text-xl mt-10">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-        deserunt illum mollitia officiis qui exercitationem perferendis neque
-        quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-        iste, totam fugiat recusandae rerum laborum perferendis molestiae
-        aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-        ab mollitia voluptatibus, a nostrum eveniet laborum!
+      As a seasoned Android Developer, I've honed my craft in the dynamic realm of mobile application
+       development and enhancement. With 1.5 years of hands-on experience, I've recently concluded my tenure at
+        Uniflyn Softtech Pvt Ltd in Coimbatore. My journey thus far has instilled in me a deep passion for technological
+         innovation and a relentless pursuit of excellence. Now, I eagerly anticipate the next chapter 
+         of my career, where I aspire to channel my expertise into transformative projects that push the boundaries of possibility.
       </p>
 
-      <br />
-
-      <p className="text-xl">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-        pariatur, vel similique sint, nobis aspernatur ut praesentium
-        explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-        quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-        Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-        magni quo! Eum cupiditate debitis labore.
-      </p>
+    
     </div>
   </div>
   )
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,56 +1,59 @@
-import React from 'react'
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import React from "react";
+import navbar from "../assets/myApp.png";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
 
 const Portfolio = () => {
-    const portfolios = [
-      
-        {
-          id: 1,
-          src: navbar,
-        },
-    
-      ];
-    
-      return (
-        <div
-          name="portfolio"
-          className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
-        >
-          <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-            <div className="pb-8">
-              <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-                Projects
-              </p>
-              <p className="py-6">Check out some of my work right here</p>
-            </div>
-    
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-              {portfolios.map(({ id, src }) => (
-                <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-                  <img
-                    src={src}
-                    alt=""
-                    className="rounded-md duration-200 hover:scale-105"
-                  />
-                  <div className="flex items-center justify-center">
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                      Demo
-                    </button>
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                      Code
-                    </button>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      );
-    };
+  const portfolios = [
+    {
+      id: 1,
+      src: navbar,
+      link: "https://play.google.com/store/apps/details?id=com.ott.rhyfill&pcampaignid=web_share",
+      title: "VDOJAR",
+    },
+  ];
 
-export default Portfolio
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
+          {portfolios.map(({ id, src, link, title }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-3xl  hover:scale-105 bg-slate-900">
+              <img
+                src={src}
+                alt=""
+                className="rounded-3xl p-2 duration-200"
+              />
+              <h4 className="text-2xl pl-4 mb-2 mt-2 font-bold">{title}</h4>
+              <div className="flex items-center justify-center">
+                <div className="mb-5 hover:scale-105">
+                  <a href={link} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-green-500 to-green-500 cursor-pointer">
+                    Play Store
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
